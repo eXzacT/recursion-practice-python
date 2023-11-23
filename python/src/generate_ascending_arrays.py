07d072fec4a7ddf5f83f4e5bb49ed8e57dfa9449
eXzacT <damjan.andelkovic1@gmail.com>
2023-11-23 20:29:40 +0100

    recursion practice day 9

---
+++ python/src/generate_ascending_arrays.py
@@ -0,0 +1,32 @@
+# Given two sorted arrays A and B, generate all possible arrays
+# such that the first element is taken from A then from B then from A,
+# and so on in increasing order till the arrays are exhausted.
+# The generated arrays should end with an element from B.
+
+def generate_ascending_arrays_from_arrays_rec(A, B):
+    result = []
+    len_A = len(A)
+    len_B = len(B)
+
+    def helper(idx_a: int, idx_b: int, is_A_turn: bool, last_used: int,
+               current_array: list[int]):
+
+        if is_A_turn:
+            for i in range(idx_a, len_A):
+                if A[i] > last_used:
+                    helper(i+1, 0, not is_A_turn,
+                           A[i], current_array + [A[i]])
+        else:
+            for i in range(idx_b, len_B):
+                if B[i] > last_used:
+                    new_array = current_array + [B[i]]
+                    result.append(new_array)
+                    helper(0, i+1, not is_A_turn,
+                           B[i], new_array)
+    helper(0, 0, True, float('-inf'), [])
+    return result
+
+
+A = [10, 15, 25]
+B = [1, 5, 20, 30]
+print(generate_ascending_arrays_from_arrays_rec(A, B))
---
+++ python/src/one_to_n_binary_alternating_1_bits.py
@@ -0,0 +1,65 @@
+# 1 to n bit numbers with no consecutive 1s in binary representation
+
+def generate_no_consecutive_ones(n: int) -> list[str]:
+    if n <= 0:
+        return ['']
+
+    binary_arr = ['0']
+    num = 1
+    while num < 1 << n:
+        bin_num = bin(num)[2:]
+        if '11' not in bin_num:
+            binary_arr.append(bin_num)
+        num += 1
+
+    return binary_arr
+
+
+def generate_no_consecutive_ones_v2(n: int) -> list[str]:
+    if n <= 0:
+        return ['']
+
+    ending_in_0 = ['0']
+    ending_in_1 = ['1']
+
+    for _ in range(1, n):
+        # New strings ending in 0 are previous strings ending in 0 or 1 followed by 0
+        new_ending_in_0 = [s + '0' for s in ending_in_0 + ending_in_1]
+
+        # New strings ending in 1 are previous strings ending in 0 followed by 1
+        new_ending_in_1 = [s + '1' for s in ending_in_0]
+
+        ending_in_0, ending_in_1 = new_ending_in_0, new_ending_in_1
+
+    return ending_in_0 + ending_in_1
+
+
+def generate_no_consecutive_ones_rec(n: int) -> list[str]:
+    if n <= 0:
+        return ['']
+
+    binary_arr = ['0']
+
+    def helper(base):
+        if len(base) > n:
+            return
+
+        if base.count('1') >= 1:
+            temp = base.lstrip('0')
+            if temp not in binary_arr:
+                binary_arr.append(temp)
+
+        # Add 0 for both cases, only add '1' if it's currently '0'
+        helper('0'+base)
+        if base[0] == '0':
+            helper('1'+base)
+
+    helper('0')
+    helper('1')
+
+    return binary_arr
+
+
+print(generate_no_consecutive_ones(4))
+print(generate_no_consecutive_ones_v2(4))
+print(generate_no_consecutive_ones_rec(4))
