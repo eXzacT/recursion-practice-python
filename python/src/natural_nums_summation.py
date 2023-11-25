890a0641d653f0050b02258a79fd378f7f9fa451
eXzacT <damjan.andelkovic1@gmail.com>
2023-11-25 19:53:19 +0100

    recursion practice day 11

--- python/src/natural_nums_summation.py
+++ python/src/natural_nums_summation.py
@@ -1,1 +1,29 @@
 # Find m-th summation of first n natural numbers
+
+def natural_nums_summation_iter(m: int, n: int) -> int:
+    for _ in range(1, m+1):
+        nums_sum = 0
+        for j in range(1, n+1):
+            nums_sum += j
+        n = nums_sum
+    return n
+
+
+def natural_nums_summation_rec(m: int, n: int) -> int:
+    if m == 0:
+        return n
+    n = sum(range(1, n+1))
+
+    return natural_nums_summation_rec(m-1, n)
+
+
+def natural_nums_summation_rec_v3(m: int, n: int):
+    if m == 1:
+        return (n * (n + 1) // 2)
+    sum_nums = natural_nums_summation_rec_v3(m - 1, n)
+    return (sum_nums * (sum_nums + 1) // 2)
+
+
+print(natural_nums_summation_iter(4, 5))
+print(natural_nums_summation_rec(4, 5))
+print(natural_nums_summation_rec_v3(4, 5))
--- python/src/sum_of_unique_natural.py
+++
@@ -1,5 +0,0 @@
-# Find ways a number can be expressed as sum of
-# n-th power of unique natural numbers
-
-def sum_of_unique_natural_to_nth(num: int):
-    
